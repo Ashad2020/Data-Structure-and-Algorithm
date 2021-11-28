fd2ff180499bfc9b14a6d66f3e03b01ec5413e09
Ashad <mdashadnstu97@gmail.com>
2021-11-28 10:37:51 +0600

    Recursive Display of Linked list in reverse order

---
+++ RecursiveDisplay.c
@@ -0,0 +1,43 @@
+#include<stdio.h>
+
+struct Node
+{
+    int data;
+    struct Node *next;
+}* first = NULL;
+
+void create(int A[], int n)
+{
+   int i;
+   struct Node *t, *last;
+   first =(struct Node *)malloc(sizeof(struct Node));
+   first-> data = A[0];
+   first-> next = NULL;
+   last = first;
+
+   for(i = 1; i< n; i++)
+   {
+       t =(struct Node *)malloc(sizeof(struct Node));
+       t -> data = A[i];
+       t -> next = NULL;
+       last -> next = t;
+       last = t;
+   }
+}
+
+void display (struct Node *p)
+{
+    if(p != NULL)
+    {
+        display(p -> next);
+        printf("%d\n", p -> data);
+    }
+}
+int main()
+{
+    int A[] = {0,01,1,2,3,4};
+    create(A, 6);
+    display(first);
+    return 0;
+}
+
