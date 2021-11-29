b71d83607c8207941abd5ca20c8a8260ade6abfd
Ashad <mdashadnstu97@gmail.com>
2021-11-29 22:15:39 +0600

    Sum of all elements of a list using recursion

---
+++ sumOfAllElements.c
@@ -0,0 +1,47 @@
+#include<stdio.h>
+
+struct Node
+{
+    int data;
+    struct Node *next;
+}* first = NULL;
+
+void create(int A[], int n)
+{
+   int i;
+   struct Node *t, *last;
+   first =(struct Node *)malloc(sizeof(struct Node));
+   first-> data = A[0];
+   first-> next = NULL;
+   last = first;
+
+   for(i = 1; i< n; i++)
+   {
+       t =(struct Node *)malloc(sizeof(struct Node));
+       t -> data = A[i];
+       t -> next = NULL;
+       last -> next = t;
+       last = t;
+   }
+}
+
+int countNodes (struct Node *p)
+{
+   if(p == NULL)
+   {
+       return 0;
+   }
+   else
+   {
+       return countNodes(p -> next) + p -> data;
+   }
+}
+int main()
+{
+    int A[] = {3,01,1,1,1,1,1};
+    create(A, 7);
+    int result = countNodes(first);
+    printf("%d",result);
+    return 0;
+}
+
